4f49ad03cd81e949fb6ee43c6ce9a15b7f8b5535
bcauchon <bcauchon@gmail.com>
2019-09-21 16:08:05 -0700

    Delete commented out code

--- src/components/DayForecastList.js
+++ src/components/DayForecastList.js
@@ -12,55 +12,7 @@ function DayForecastList() {
       low: Math.round(data.temperatureLow),
     });
   });
-    // {
-    //   day:'Sat',
-    //   description: 'Mostly Cloudy',
-    //   high: 71,
-    //   low: 58,
-    // },
-    // {
-    //   day:'Sun',
-    //   description: 'Rain',
-    //   high: 66,
-    //   low: 53,
-    // },
-    // {
-    //   day:'Mon',
-    //   description: 'Partly Cloudy',
-    //   high: 69,
-    //   low: 56,
-    // },
-    // {
-    //   day:'Tue',
-    //   description: 'Partly Cloudy',
-    //   high: 69,
-    //   low: 52,
-    // },
-    // {
-    //   day:'Wed',
-    //   description: 'Partly Cloudy',
-    //   high: 67,
-    //   low: 52,
-    // },
-    // {
-    //   day:'Thu',
-    //   description: 'Showers',
-    //   high: 61,
-    //   low: 49,
-    // },
-    // {
-    //   day:'Fri',
-    //   description: 'Showers',
-    //   high: 59,
-    //   low: 47,
-    // },
-    // {
-    //   day:'Sat',
-    //   description: 'Mostly Cloudy',
-    //   high: 58,
-    //   low: 46,
-    // },
-  // ]
+  
   return (
     <div className="DayForecastList">
       {dayForecastList.map(
