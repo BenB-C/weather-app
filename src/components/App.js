c253dc6d59f8077a207541b1ee13daacd1bf731b
bcauchon <bcauchon@gmail.com>
2019-09-21 11:28:07 -0700

    Restructure file tree

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const location = {
+    city: 'Portland',
+    state: 'OR',
+    zip: 97205
+  }
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
