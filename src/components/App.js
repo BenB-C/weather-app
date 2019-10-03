027066dba54c049032070aff5b2cb9e155e1805c
benb-c <bcauchon@gmail.com>
2019-10-03 16:55:17 -0700

    Move HourlyConditions below DayForecastList

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,8 @@ function App() {
       <SearchBar />
       <Location />
       <CurrentConditions />
-      <HourlyConditions />
       <DayForecastList />
+      <HourlyConditions />
     </div>
   );
 }
